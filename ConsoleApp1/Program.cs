165dbc3a3b4366375d4b21e8e93b19cdf93e291a
Jesus_JM_UJI <al427616@uji.es>
2023-02-01 16:11:03 +0100

    Add CSharp test item

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program 
+{ 
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hola, me dices tu nombre: ");
+        string name = Console.ReadLine();
+        if (name == "Raul")
+        {
+            Console.WriteLine(name + " " + "guapo");
+        }
+        else if (name == "Alonso")
+        {
+            Console.WriteLine(name + " " + "buen dm :), dale agro a ");
+        }
+        else 
+        {
+            Console.WriteLine("Bienvenido a C#, " + name);
+        }
+        
+        Console.ReadKey();
+    }
+}
