1b727019848d2c5d01e03b61f370b101ba86fa9a
JesusJMUJI <al427616@uji.es>
2023-02-06 22:55:32 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program 
+{ 
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hola, me dices tu nombre: ");
+        string name = Console.ReadLine();
+        if (name == "Raul")
+        {
+            Console.WriteLine(name + " " + "guapo");
+        }
+        else if (name == "Alonso")
+        {
+            Console.WriteLine(name + " " + "buen dm :), dale agro a Raul");
+        }
+        else if (name == "Karl")
+        {
+            Console.WriteLine(name + "???" + " WHICH KARL IM CONFUSED");
+            Console.WriteLine($"{name} {name} {name} {name} {name} {name} {name}");
+        }
+        else 
+        {
+            Console.WriteLine("Bienvenido a C#, " + name);
+        }
+        
+        Console.ReadKey();
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name1 = Console.ReadLine();
+        string name2 = Console.ReadLine();
+        string name3 = Console.ReadLine();
+        
+        
+    }
+}
