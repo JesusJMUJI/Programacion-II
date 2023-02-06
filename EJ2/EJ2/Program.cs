e80477054876f84489723e1fcf6df814e1a257f4
JesusJMUJI <al427616@uji.es>
2023-02-06 23:15:28 +0100

    sdsadassa

---
+++ EJ2/EJ2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+class EJ2{
+
+    static void Main(string[] args) 
+    {
+
+        Console.WriteLine("Write three numbers and the biggest one will be shown: ");
+        
+
+        int name1 = Convert.ToInt32(Console.ReadLine());
+        int name2 = Convert.ToInt32(Console.ReadLine());
+        int name3 = Convert.ToInt32(Console.ReadLine());
+
+        int largestNumber = name1;
+        if (name2 > largestNumber)
+        {
+            largestNumber = name2;
+        }
+        if (name3 > largestNumber)
+        {
+            largestNumber = name3;
+        }
+	 
+        Console.WriteLine("The biggest number is: " + largestNumber);
+    }
+}
