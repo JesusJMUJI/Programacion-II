dfad8351039183d6c7c5366409be9f70af22f3d1
Jesus_JM_UJI <al427616@uji.es>
2023-02-06 22:54:52 +0100

    ighjjhghjhgj

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string arg)
+    {
+        Console.WriteLine("EScribe tres nombres: ");
+
+
+        int name1 = Convert.ToInt32(Console.ReadLine());
+        int name2 = Convert.ToInt32(Console.ReadLine());
+        int name3 = Convert.ToInt32(Console.ReadLine());
+
+        int largestNumber = name1;
+
+        if (largestNumber > name2)
+        {
+            largestNumber = name2;
+        }
+
+        if (largestNumber > name3)
+        {
+            largestNumber = name3;
+        }
+    }
+}
