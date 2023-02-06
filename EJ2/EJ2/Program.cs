079d101fe3f85b674c719b53ab6855696791ce0b
JesusJMUJI <al427616@uji.es>
2023-02-07 00:49:13 +0100

    d

--- EJ2/EJ2/Program.cs
+++ EJ2/EJ2/Program.cs
@@ -1,26 +1,24 @@
 ﻿using System;
 
+class Ej2{
 
-class EJ2{
-
-    static void Main(string[] args) 
-    {
-
+    static void Main(string[] args) {
+        
         Console.WriteLine("Write three numbers and the biggest one will be shown: ");
         
 
-        int name1 = Convert.ToInt32(Console.ReadLine());
-        int name2 = Convert.ToInt32(Console.ReadLine());
-        int name3 = Convert.ToInt32(Console.ReadLine());
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        int number3 = Convert.ToInt32(Console.ReadLine());
 
-        int largestNumber = name1;
-        if (name2 > largestNumber)
+        int largestNumber = number1;
+        if (number2 > largestNumber)
         {
-            largestNumber = name2;
+            largestNumber = number2;
         }
-        if (name3 > largestNumber)
+        if (number3 > largestNumber)
         {
-            largestNumber = name3;
+            largestNumber = number3;
         }
 	 
         Console.WriteLine("The biggest number is: " + largestNumber);
