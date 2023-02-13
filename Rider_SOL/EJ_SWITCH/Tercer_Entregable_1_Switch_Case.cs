cf82a13a69d1a269177df04c06888236c6a4ef01
JesusJMUJI <al427616@uji.es>
2023-02-13 09:57:44 +0100

    Añadir ejercicios

---
+++ Rider_SOL/EJ_SWITCH/Tercer_Entregable_1_Switch_Case.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class SwitchTest
+{
+    static void Main()
+    {
+        Console.WriteLine("Say a country's name and i'll tell you what language is spoken");
+
+        string wordInput = Console.ReadLine();
+
+        switch (wordInput)
+        {
+            case "Germany":
+                Console.WriteLine($"In {wordInput}, the language 'German' is spoken");
+                break;
+            case "Austria":
+                Console.WriteLine($"In {wordInput}, the language 'German' is spoken");
+                break;
+            case "Switzerland":
+                Console.WriteLine($"In {wordInput}, the language 'German' is spoken");
+                break;
+            case "United Kingdom":
+                Console.WriteLine($"In {wordInput}, the language 'English' is spoken");
+                break;
+            case "USA":
+                Console.WriteLine($"In {wordInput}, the language 'English' is spoken");
+                break;
+            case "Argentina":
+                Console.WriteLine($"In {wordInput}, the language 'Spanish' is spoken");
+                break;
+            case "Cuba":
+                Console.WriteLine($"In {wordInput}, the language 'Spanish' is spoken");
+                break;
+            case "Venezuela":
+                Console.WriteLine($"In {wordInput}, the language 'Spanish' is spoken");
+                break;
+            default:
+                Console.WriteLine("Option not valid, the input is case sensitive or the country is not in the list");
+                break;
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ Rider_SOL/Tercer_Entregable_2/SumaDivisores.cs
@@ -0,0 +1,27 @@
+﻿namespace Tercer_Entregable_2;
+
+class SumaDivisores
+{
+    static void Main()
+    {
+        int number;
+        int sum = 0;
+        int i = 1;
+    
+        Console.Write("Write a number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        do
+        {
+            if (number % i == 0)
+            {
+                sum += i;
+            }
+
+            i++;
+        } while (i <= number);
+    
+        Console.WriteLine($"The sum of the divisors of {number} is {sum}");
+        Console.ReadKey();
+    }
+}
