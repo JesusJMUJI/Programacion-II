5084e7bd59c8fff86aa93028a9b10f545c20e69a
JesusJMUJI <al427616@uji.es>
2023-02-18 13:09:39 +0100

    Entregable 4
    * Ejercicio 1
    * Ejercicio 2
    * Ejercicio 3

---
+++ Cuarto_Entregable_2/Cuarto_Entregable_2.cs
@@ -0,0 +1,45 @@
+﻿class OrdenarMatrizMayorMenor
+{
+    static void Main()
+    {
+        double[,] matriz =
+        {
+            {-5.4, -3.2, -3.1, 2.5, 3.6},
+            { 3.2, 4.8, 4.9, 5.2, 6.9},
+            {-1.4, -1.2, -1.0, -0.5, -0.1}
+        };
+
+        int[] posiciones = OrdenarMatriz(matriz);
+
+        for (int i = 0; i < posiciones.Length; i++)
+        {
+            Console.WriteLine("Fila {0}: {1}", i, posiciones[i]);
+        }
+
+        Console.ReadLine();
+        
+    }
+
+    public static int[] OrdenarMatriz(double[,] matriz)
+    {
+        int n = matriz.GetLength(0); //filas
+        int m = matriz.GetLength(1); //columna
+        int[] resultado = new int[n];
+
+        for (int fila= 0; fila < n; fila++) 
+        {
+            int soloNegativos = -1;
+
+            for (int columna = 0; columna < m; columna++)
+            {
+                if (matriz[fila, columna] >= 0)
+                {
+                    soloNegativos = columna;
+                    break;
+                }
+            }
+            resultado[fila] = soloNegativos;
+        }
+        return resultado;
+    }
+}
