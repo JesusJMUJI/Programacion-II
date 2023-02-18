7f5dfe4857be0416dd3e27654a3f9765a83baa68
JesusJMUJI <al427616@uji.es>
2023-02-18 13:09:39 +0100

    Entrgable_4
    * Ejercicio 1
    * Ejercicio 2
    * Ejercicio 3

---
+++ Cuarto_Entregable_3/Cuarto_Entregable_3.cs
@@ -0,0 +1,38 @@
+﻿class Entregable_3
+{
+    static void Main()
+    {
+        Console.Write("Escribe la fila que quieres poner a 0:");
+        int filaConcreta = Convert.ToInt32(Console.ReadLine());
+        filaConcreta --;
+
+        int[,] matriz =
+        {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        MatrizAsignar0(matriz, filaConcreta);
+
+        for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                Console.Write(matriz[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
+    }
+
+    public static void MatrizAsignar0(int[,] matriz, int fila0)
+    { 
+
+        for (int j = 0; j < matriz.GetLength(1); j++)
+        {
+            matriz[fila0, j] = 0;
+        }
+    }
+}
---
+++ Entregables_VS/Cuarto_Entregable_1.cs
@@ -0,0 +1,46 @@
+﻿class Amigos
+{
+
+    static void Main()
+    {
+        Console.Write("Escribe el primer numero:"); int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Escribe el segundo numero"); int num2 = Convert.ToInt32(Console.ReadLine());
+
+        bool sonAmigos = SonAmigos(num1, num2);
+
+        if (sonAmigos)
+        {
+            Console.WriteLine("{0} y {1} son amigos", num1, num2);
+        }
+        else
+        {
+            Console.WriteLine("{0} y {1} no son amigos", num1, num2);
+        }
+
+        Console.ReadLine();
+    }
+
+    public static bool SonAmigos(int num1, int num2)
+    {
+        int sum1 = 0;
+        int sum2 = 0;
+        for (int i = 1; i <= num1 / 2; i++) 
+            // Se divide el num1 entre dos para optimizar el bucle,
+            // según el ejemplo dado en clase,
+            // no pueden haber divisores mayores que la mitad del número
+        {
+            if (num1 % i == 0)
+            {
+                sum1 += i;
+            }
+        }
+        for (int i = 1; i <= num2 / 2; i++)
+        {
+            if (num2 % i == 0)
+            {
+                sum2 += i;
+            }
+        }
+        return sum1 == num2 && sum2 == num1;
+    }
+}
