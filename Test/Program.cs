a218b9a916b30643401c1b5b1b5274a7c71bdb9c
Jesus_JM_UJI <al427616@uji.es>
2023-02-27 15:32:03 +0100

    Ni idea de esto

---
+++ Test/Program.cs
@@ -0,0 +1,18 @@
+﻿class Programa
+{
+    public void Selcccion(int[] v)
+    {
+        int[,] ints;
+        for (int i = 0; i < v.Length - 1; i++)
+        {
+            int posicion = i;
+            for (int j = i++; j < v.Length; j++ )
+            {
+                if (v[j] < v[posicion]) posicion = j;
+            }
+            int aux = v[i];
+            v[i] = v[posicion];
+            v[posicion] = aux;
+        }
+    }
+}
