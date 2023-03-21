17d7a9ec0f2d09afd8b830884adce2e2d3f1de3d
Jesus_JM_UJI <al427616@uji.es>
2023-03-21 09:51:29 +0100

    Comenzar ej7

---
+++ Septimo_Entregable/Ej_2/7.2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_2
+{
+    internal class Program
+    {
+        public class Pelota
+        {
+            private double radio;
+            private string color;
+            
+            public Pelota(double radio, string color)
+            {
+                this.radio = radio;
+                this.color = color;
+            }
+        }
+    }
+}
---
+++ Septimo_Entregable/Septimo_Entregable/7.1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Septimo_Entregable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemigo odioso = new Enemigo("Odioso",
+                             "Espada");
+            Enemigo brujo = odioso;
+            brujo.SetNombre("Brujo");
+            brujo.SetArma(“Hechizos");
+            Console.WriteLine(odioso.GetLucha());
+            Console.WriteLine(brujo.GetLucha());
+
+        }
+        public struct Enemigo
+        {
+            private String nombre;
+            private String arma;
+            public Enemigo(String nombre, String arma)
+            {
+                this.nombre = nombre;
+                this.arma = arma;
+            }
+            public String Lucha()
+            {
+                return "Me llamo “+nombre+“ y ataco con “+arma;
+            }
+            public String GetNombre() { return nombre; }
+            public String GetArma() { return nombre; }
+            public void SetNombre(String n) { nombre = n; }
+            public void SetArma(String a) { arma = a; }
+
+        }
+        public class Villano
+        {
+            /// El mismo contenido que el struct Enemigo,
+            ///   es decir, los atributos nombre y arma, el 
+            ///   constructor y los métodos públicos Lucha, 
+            ///   GetNombre, GetArma, SetNombre y SetArma.
+        }
+
+    }
+}
