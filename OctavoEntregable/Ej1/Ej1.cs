5b58cb6acc14eae54e50ea894783574bfdd041fe
JesusJMUJI <al427616@uji.es>
2023-03-25 13:35:51 +0100

    Ej1 y Ej2 del octavo entregable

---
+++ OctavoEntregable/Ej1/Ej1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Ej1
+{
+    internal class Ej1
+    {
+        public static void Main()
+        {
+            Punto[] puntos = new Punto[] { new Punto(12, 41), new Punto(44, 53) };
+
+            Console.WriteLine("Array antes de OperacionDoblez:");
+            foreach (Punto punto in puntos)
+            {
+                Console.WriteLine($"({punto.X}, {punto.Y})");
+            }
+
+            OperacionesConPuntos.OperacionDoblez(puntos);
+
+            Console.WriteLine("Array después de OperacionDoblez:");
+            foreach (Punto punto in puntos)
+            {
+                Console.WriteLine($"({punto.X}, {punto.Y})");
+            }
+
+            Console.WriteLine("Array antes de OperacionDoblezCopia:");
+            foreach (Punto punto in puntos)
+            {
+                Console.WriteLine($"({punto.X}, {punto.Y})");
+            }
+
+            Punto[] copia = OperacionesConPuntos.OperacionDoblezCopia(puntos);
+
+            Console.WriteLine("Array después de OperacionDoblezCopia:");
+            foreach (Punto punto in copia)
+            {
+                Console.WriteLine($"({punto.X}, {punto.Y})");
+            }
+
+            Console.WriteLine("Valor del array original:");
+            foreach (Punto punto in puntos)
+            {
+                Console.WriteLine($"({punto.X}, {punto.Y})");
+            }
+
+        }
+
+        public class Punto
+        {
+            private int x;
+            private int y;
+
+            public Punto(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public int X
+            {
+                get { return x; }
+                set { x = value; }
+            }
+
+            public int Y
+            {
+                get { return y; }
+                set { y = value; }
+            }
+        }
+
+        public static class OperacionesConPuntos
+        {
+            public static void OperacionDoblez(Punto[] puntos)
+            {
+                foreach (Punto punto in puntos)
+                {
+                    punto.X *= 2;
+                    punto.Y /= 2;
+                }
+            }
+
+            public static Punto[] OperacionDoblezCopia(Punto[] puntos)
+            {
+                Punto[] copia = new Punto[puntos.Length];
+                for (int i = 0; i < puntos.Length; i++)
+                {
+                    copia[i] = new Punto(puntos[i].X * 2, puntos[i].Y / 2);
+                }
+                return copia;
+            }
+        }
+
+    }
+}
---
+++ OctavoEntregable/Ej2/Ej2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Ej2
+{
+    internal class Ej2
+    {
+
+        public class Persona
+        {
+            public string DNI { get; set; }
+            public string Nombre { get; set; }
+            public string Direccion { get; set; }
+
+            public Persona(string dni, string nombre, string direccion)
+            {
+                DNI = dni;
+                Nombre = nombre;
+                Direccion = direccion;
+            }
+        }
+
+        public class CuentaBancaria
+        {
+            public int NumeroCuenta { get; set; }
+            public Persona Titular { get; set; }
+            public decimal Saldo { get; set; }
+
+            public CuentaBancaria(int numeroCuenta, Persona titular, decimal saldo)
+            {
+                NumeroCuenta = numeroCuenta;
+                Titular = titular;
+                Saldo = saldo;
+            }
+        }
+
+        public class Banco
+        {
+            public string Direccion { get; set; }
+            public string Codigo { get; set; }
+            public string Nombre { get; set; }
+            private readonly CuentaBancaria[] cuentasBancarias = new CuentaBancaria[1000];
+            public int cantidadCuentasBancarias;
+
+            public Banco(string direccion, string codigo, string nombre)
+            {
+                Direccion = direccion;
+                Codigo = codigo;
+                Nombre = nombre;
+            }
+
+            public void AgregarCuenta(CuentaBancaria cuenta)
+            {
+                if (cantidadCuentasBancarias >= 1000)
+                {
+                    Console.WriteLine("No pueden haber mas de 100 cuentas");
+                }
+                else
+                {
+                    cuentasBancarias[cantidadCuentasBancarias] = cuenta;
+                    cantidadCuentasBancarias++;
+                }
+            }
+
+            public int ContarCuentas()
+            {
+                int totalClientes = 0;
+
+                for (int i = 0; i < cuentasBancarias.Length; i++)
+                {
+                    totalClientes++;
+                }
+                return totalClientes;
+            }
+
+            public decimal Activos()
+            {
+                decimal totalActivos = 0;
+                for (int i = 0; i < cantidadCuentasBancarias; i++)
+                {
+                    totalActivos += cuentasBancarias[i].Saldo;
+                }
+                return totalActivos;
+            }
+        }
+    }
+}
